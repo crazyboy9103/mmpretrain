11e84ff3454df5a07071df53ded645bd7b4b188d
crazyboy9103 <gil9103@gmail.com>
2024-07-10 05:07:06 +0000

    efficientnet_v2

--- configs/neurocle/efficientnet_v2/efficientnetv2-b3_b8_hanrim.py
+++ configs/neurocle/efficientnet_v2/efficientnetv2-b3_b8_hanrim.py
@@ -1,12 +1,16 @@
 _base_ = [
-    '../_base_/models/efficientnet_v2/efficientnetv2_b0.py',
+    '../_base_/models/efficientnet_v2/efficientnetv2_b3.py',
     '../_base_/datasets/hanrim_bs8.py',
     '../_base_/schedules/hanrim.py',
     '../_base_/default_runtime.py',
 ]
 
 # model setting
-model = dict(backbone=dict(arch='b3'), head=dict(in_channels=1536, num_classes={{_base_.data_preprocessor.num_classes}}))
+model = dict(
+    head=dict(
+        num_classes={{_base_.data_preprocessor.num_classes}}
+    )
+)
 
 vis_backends = [
     dict(type='LocalVisBackend'),
