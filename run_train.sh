9d294f45218a65be38c92e7edd9fd778359483af
crazyboy9103 <gil9103@gmail.com>
2024-07-10 06:39:53 +0000

    run_train

--- run_train.sh
+++ run_train.sh
@@ -11,9 +11,5 @@ models=("swin-tiny" "convnext-tiny" "deit-small" "efficientnet-b4" "efficientnet
 for i in "${!folders[@]}"; do
     model="${models[$i]}"
     folder="${folders[$i]}"
-    log_dir="logs/$data/$folder"
-    if [ ! -d "$log_dir" ]; then
-        mkdir -p "$log_dir"
-    fi
-    python tools/train.py configs/neurocle/"$folder"/"$model"_b8_"$data".py --work-dir "./work_dirs/$data/$model" 2>&1 | tee "$log_dir/$model.log"
+    python tools/train.py configs/neurocle/"$folder"/"$model"_b8_"$data".py --work-dir "./work_dirs/$data/$model"
 done
